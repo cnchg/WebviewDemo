4b4bbba3f0558957ee1c1d6c3cdaf65f53f206dd
“Ori <chandleretienne@gmail.com>
2016-12-18 22:34:50 -0600

    Initial commit

---
+++ app/src/main/java/com/tricloudcommunications/ce/webviewdemo/MainActivity.java
@@ -0,0 +1,57 @@
+package com.tricloudcommunications.ce.webviewdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        //webView.loadUrl("http://tricloudcommunications.com/login.php");
+        //webView.loadUrl("http://google.com");
+
+        /** Note: You can also send data, like our own html data loaded in the view **/
+        webView.loadData("<html><body><h1>Hello There, from HTML</h1><p>This is my site! A HTML page loaded in this view</p></body></html>", "text/html", "UTF-8");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
